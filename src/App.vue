f3607e547c88111ead6ff4b0539a7e25d4b8da50
Colin Brayton <gringolalia@gmail.com>
2020-02-11 17:33:49 -0300

    radiant newnewss

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,85 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+#app
+	.page-container.md-layout-column
+		md-toolbar.md-primary
+			md-button.md-icon-button(@click='showNavigation = true')
+				md-icon menu
+			span.md-title 
+				i.fas.fa-sun.fa-2x.mr-2.ml-0
+				| Engenho Novo LLC
+			.md-toolbar-section-end
+				md-button(@click='showSidepanel = true') Collateral Dommage
+		md-drawer(:md-active.sync='showNavigation' md-swipeable)
+			md-toolbar.md-transparent(md-elevation='0')
+				span.md-title The Oca Engenhoca
+			md-list
+				md-list-item
+					md-icon move_to_inbox
+					span.md-list-item-text Inbox
+				md-list-item
+					md-icon send
+					span.md-list-item-text Sent Mail
+				md-list-item
+					md-icon delete
+					span.md-list-item-text Trash
+				md-list-item
+					md-icon error
+					span.md-list-item-text Spam
+		md-drawer.md-right(:md-active.sync='showSidepanel')
+			md-toolbar.md-transparent(md-elevation='0')
+				span.md-title Selections
+			md-list
+				md-list-item
+					span.md-list-item-text Abbey Christansen
+					md-button.md-icon-button.md-list-action
+						md-icon.md-primary chat_bubble
+				md-list-item
+					span.md-list-item-text Alex Nelson
+					md-button.md-icon-button.md-list-action
+						md-icon.md-primary chat_bubble
+				md-list-item
+					span.md-list-item-text Mary Johnson
+					md-button.md-icon-button.md-list-action
+						md-icon chat_bubble
+		md-content
+			router-view
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    name: 'Temporary',
+    data: () => ({
+      showNavigation: false,
+      showSidepanel: false
+    })
   }
-}
+</script>
+
+<style lang="scss">
+
+@import "~vue-material/dist/theme/engine";
+
+@include md-register-theme("default", (
+	primary: #607d8b,
+	accent: #2a3439, 
+));
+
+@import "~vue-material/dist/theme/all"; 
+
+.page-container {
+    min-height: 300px;
+    overflow: hidden;
+    position: relative;
+    border: 1px solid rgba(#000, .12);
+  }
+
+   // Demo purposes only
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+
+  .md-content {
+    padding: 16px;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+.home
+	img(alt='Vue logo' src='../assets/anarchy.svg' width='125px')
+	HelloWorld(msg='Good Morning, Midnight').
+	.md-display-2.my-4 Ban the Bomb
+	.md-display-1.mt-2.mb-4 Burn the Brassiere
+	md-button.md-dense.md-raised.md-primary Pressed for Time
+	md-button.md-dense.md-raised.md-accent Press Your Luck
+		
 </template>
 
 <script>
@@ -10,9 +14,14 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	components: {
+		HelloWorld
+	}
 }
 </script>
+
+<style>
+.home {
+	min-height: 100vh;
+}
