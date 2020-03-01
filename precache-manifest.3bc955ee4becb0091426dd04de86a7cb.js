9c939c81367e0c10f16f74bef5881aec6549f055
Colin Brayton <gringolalia@gmail.com>
2020-03-01 12:49:50 -0300

    deploy

---
+++ precache-manifest.3bc955ee4becb0091426dd04de86a7cb.js
@@ -0,0 +1,322 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f0cfd0dfa7fb685406cb08a04aa01b",
+    "url": "/in-our-vue-it-matters/assets"
+  },
+  {
+    "revision": "4cf2d64e44205fe628ddd534e1151b58",
+    "url": "/in-our-vue-it-matters/beautilities/.git/HEAD"
+  },
+  {
+    "revision": "e7ca6d41045e380e483164db438b470f",
+    "url": "/in-our-vue-it-matters/beautilities/.git/config"
+  },
+  {
+    "revision": "a0a7c3fff21f2aea3cfa1d0316dd816c",
+    "url": "/in-our-vue-it-matters/beautilities/.git/description"
+  },
+  {
+    "revision": "ce562e08d8098926a3862fc6e7905199",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/applypatch-msg.sample"
+  },
+  {
+    "revision": "579a3c1e12a1e74a98169175fb913012",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/commit-msg.sample"
+  },
+  {
+    "revision": "2b7ea5cee3c49ff53d41e00785eb974c",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/post-update.sample"
+  },
+  {
+    "revision": "054f9ffb8bfe04a599751cc757226dda",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/pre-applypatch.sample"
+  },
+  {
+    "revision": "01b1688f97f94776baae85d77b06048b",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/pre-commit.sample"
+  },
+  {
+    "revision": "3c5989301dd4b949dfa1f43738a22819",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/pre-push.sample"
+  },
+  {
+    "revision": "3ff6ba9cf6d8e5332978e057559b5562",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/pre-rebase.sample"
+  },
+  {
+    "revision": "2ad18ec82c20af7b5926ed9cea6aeedd",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/pre-receive.sample"
+  },
+  {
+    "revision": "7dfe15854212a30f346da5255c1d794b",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/prepare-commit-msg.sample"
+  },
+  {
+    "revision": "517f14b9239689dff8bda3022ebd9004",
+    "url": "/in-our-vue-it-matters/beautilities/.git/hooks/update.sample"
+  },
+  {
+    "revision": "874adbbc7be8f09d371bf11c0fe4efab",
+    "url": "/in-our-vue-it-matters/beautilities/.git/index"
+  },
+  {
+    "revision": "036208b4a1ab4a235d75c181e685e5a3",
+    "url": "/in-our-vue-it-matters/beautilities/.git/info/exclude"
+  },
+  {
+    "revision": "515fb625df5a32fb00036d22bc610721",
+    "url": "/in-our-vue-it-matters/beautilities/.git/logs/HEAD"
+  },
+  {
+    "revision": "515fb625df5a32fb00036d22bc610721",
+    "url": "/in-our-vue-it-matters/beautilities/.git/logs/refs/heads/master"
+  },
+  {
+    "revision": "515fb625df5a32fb00036d22bc610721",
+    "url": "/in-our-vue-it-matters/beautilities/.git/logs/refs/remotes/origin/HEAD"
+  },
+  {
+    "revision": "6aeb8a2af1dd27c632942fff7f71268c",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/00/e3e772e007161f652e14cec5de965367d369f9"
+  },
+  {
+    "revision": "6aa29a48dcf4eba0c79a76c96cca9fc3",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/02/613655bbca9e1c0f6d835c71afae10a68e0b51"
+  },
+  {
+    "revision": "7a8363b47e423ce7a616395a0a540f93",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/03/85e8555fbcd90a8bffaccb1ac188a3e1da9973"
+  },
+  {
+    "revision": "098de3eb98fcaa3c9f64f3de78bcdd88",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/07/a495ca6d77a98a9b9d29ce29c95aa11b26c9a6"
+  },
+  {
+    "revision": "3fff59147a8979438e7a79c0926746f3",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/0e/72e36e3246e4ee3bd199de7c80ad3465cc9061"
+  },
+  {
+    "revision": "dc51f6aace96d19917ebf102356f1cda",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/18/f526ed8eceaef2ff139a9e9fd6ebe95e01a3f5"
+  },
+  {
+    "revision": "0f8e897e938b1461ef9f6c8f1fa9b27b",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/1a/4d982f0006d8309ec54dd36ec40c643f8b5f94"
+  },
+  {
+    "revision": "171d0b8fcb4c3471c382c37efc01213d",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/1d/a4eddfb0c745438c7952e9e7e241291e732572"
+  },
+  {
+    "revision": "4c9f3b1443335f02e9ca73c32b0a9d82",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/1e/a9c2f366b3d736f390e8a2421d74312052d0c6"
+  },
+  {
+    "revision": "5688b4c94e401c65ec821b4be991b6a7",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/21/e380f9507d5df3286b985364170efb81251576"
+  },
+  {
+    "revision": "ffb471bca438b5087f77919608d2abf9",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/25/998929570cfbfe4ac7e92f75022958ff77a260"
+  },
+  {
+    "revision": "be20c8bfcf9fc9bf551b5f10c0b50b14",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/29/4c76d4b70d5ad79ad8066d4e88139073a80658"
+  },
+  {
+    "revision": "7ac7cc11abfb2214472099db6ef6b486",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/40/b878db5b1c97fc77049537a71bb2e249abe5dc"
+  },
+  {
+    "revision": "27dd5cd682cc4d184ea71c6212e4f291",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/44/922c725e0dfd4fbf16bab8266fee2a024f97a8"
+  },
+  {
+    "revision": "a71c9b80b4b02fa808ac93de410a7e43",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/45/7c3936e1ce47710313f722a287d4561fbc3925"
+  },
+  {
+    "revision": "62b8c5b207ddf5eaad56a2434765f7c3",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/52/4e2c4877156b74d5cd9f3f8817ae73f9943380"
+  },
+  {
+    "revision": "b4e1a51309e04ccd3ab0bcde295148c8",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/5b/ac581ed92b720fbe47b11f8c594aac5e36aea0"
+  },
+  {
+    "revision": "0192805f7294c3883826ea42e10a0a71",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/68/d09128c3b003778207a58e394c52b4f67f704f"
+  },
+  {
+    "revision": "87a2a03b176d840b69a1d528558712b6",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/68/d8efc9b569d206aff95dd71d9c230dfba441e8"
+  },
+  {
+    "revision": "ffec1ee393b528b707752cdb6cc3d687",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/72/4fb9e03ee322dc08ee04ac9d701961697c0633"
+  },
+  {
+    "revision": "6a15d993686f5b23ae29668c697d6aa7",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/79/3c7094c5529c15179827fb914e3c3e77fa09b9"
+  },
+  {
+    "revision": "0023499f9be7c5cda88c32d9d74fca70",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/7c/bb3171a297d564df37a92e96e05addde803c32"
+  },
+  {
+    "revision": "e429c3a4f272974c10326a551aa59e1b",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/7d/17f012ebc0af05419c1dea7bfa4f8ed4aba548"
+  },
+  {
+    "revision": "93ce9a0bb2e267a8c735a8e0ec7c91bb",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/7e/9d411275ab85f8220f0e78e3941b1f18637988"
+  },
+  {
+    "revision": "f8c2d40ae5ed6f9afb4416e3d16787a2",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/94/5769310733ff2d9dd7fcdda4924224152748d2"
+  },
+  {
+    "revision": "f6ca628ac59c47bd64813f6bcd274ef3",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/9b/ef602ebbe1797a79cdcb2f02bc63a95c2c17ea"
+  },
+  {
+    "revision": "2f2aa5536a14c0f34a664de45411dedd",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/a7/b9524928375317a3a33c26858e149f1f54dca6"
+  },
+  {
+    "revision": "d9fa4e78e628477038325ef7b4b49b84",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/b5/dbd60ce7d52aae96a943d5b98507b371e908c6"
+  },
+  {
+    "revision": "8519772523bec33bf3d951a7e355e825",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/b8/4e97c3b576fa12b5882e483ae92236492c8599"
+  },
+  {
+    "revision": "9e8641fe61d9f233174acf927767583f",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/ba/f38009b2731a0ed2cc21eed1f854bdd610e1c2"
+  },
+  {
+    "revision": "e4d959a9ff02ed8878280405e66728db",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/c5/c8c79771b26db89ab457ac6c26b2f1a2c02e70"
+  },
+  {
+    "revision": "56b2e020151e80afdc1a1603e6d00975",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/cf/ee4a58a89fc5b1992b046b5b6568591731ecf0"
+  },
+  {
+    "revision": "2311d1fd3ed4772bda957c62cc9baa61",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/d0/ad992ec317499339e1451337b1b51034723685"
+  },
+  {
+    "revision": "cc240eb31772eee2e21f7d02df1944ce",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/db/b69aa0183054f40fa6d88bdb0d42e8876c9118"
+  },
+  {
+    "revision": "fd6a6d8e28206eea501e7e2d08e4ab45",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/e2/0a26a49e0034c5a4a31de2dd94ca4cec37f85e"
+  },
+  {
+    "revision": "f897e0f1cf7d24d292f0641d4d5889c6",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/e7/3d59cddc85f6206445e78d8a6043b25d55570e"
+  },
+  {
+    "revision": "3ec7c4e48359729afb02e5015c67a9e0",
+    "url": "/in-our-vue-it-matters/beautilities/.git/objects/ec/d00130737d7f1e0d934fcd953c7f2f6b48d0fa"
+  },
+  {
+    "revision": "d9388ac1cbcdd9fbb0ff2b6ba85a3193",
+    "url": "/in-our-vue-it-matters/beautilities/.git/packed-refs"
+  },
+  {
+    "revision": "a855df007be37ebad6540f1deea35950",
+    "url": "/in-our-vue-it-matters/beautilities/.git/refs/heads/master"
+  },
+  {
+    "revision": "73a00957034783b7b5c8294c54cd3e12",
+    "url": "/in-our-vue-it-matters/beautilities/.git/refs/remotes/origin/HEAD"
+  },
+  {
+    "revision": "acd06b18086a0dd2ae699b1e0b775be8",
+    "url": "/in-our-vue-it-matters/beautilities/.gitignore"
+  },
+  {
+    "revision": "21208ce03eb769f0a39eb210e5048cb1",
+    "url": "/in-our-vue-it-matters/beautilities/Gruntfile.js"
+  },
+  {
+    "revision": "8f8af254998164c332c794817148cde8",
+    "url": "/in-our-vue-it-matters/beautilities/LICENSE"
+  },
+  {
+    "revision": "41e133333b72a98153285dfa31157288",
+    "url": "/in-our-vue-it-matters/beautilities/README.md"
+  },
+  {
+    "revision": "5e40fec4c9c7f075c81a530686152ad2",
+    "url": "/in-our-vue-it-matters/beautilities/dist/bootstrap-4-utilities.css"
+  },
+  {
+    "revision": "5afbfd8380658c7161ca8cee8fcd8374",
+    "url": "/in-our-vue-it-matters/beautilities/dist/bootstrap-4-utilities.min.css"
+  },
+  {
+    "revision": "55ff56bfdacad3e73421151ef49af607",
+    "url": "/in-our-vue-it-matters/beautilities/less/bootstrap-4-utilities.less"
+  },
+  {
+    "revision": "9efa37c24a10465ab7d0e734a984517f",
+    "url": "/in-our-vue-it-matters/beautilities/less/utilities/float.less"
+  },
+  {
+    "revision": "3b8e2511f5348d873c58acc1e9e5d15a",
+    "url": "/in-our-vue-it-matters/beautilities/less/utilities/spacing.less"
+  },
+  {
+    "revision": "e51aca7171e2e0049af9034e5c0a12bb",
+    "url": "/in-our-vue-it-matters/beautilities/less/utilities/text.less"
+  },
+  {
+    "revision": "1e96338277d416da6bb1a715245c3992",
+    "url": "/in-our-vue-it-matters/beautilities/less/utilities/variables.less"
+  },
+  {
+    "revision": "a1938400e871aa086cd496d0278d5539",
+    "url": "/in-our-vue-it-matters/beautilities/package.json"
+  },
+  {
+    "revision": "cdaa517a5341d1b0c2be",
+    "url": "/in-our-vue-it-matters/css/app.2c542b41.css"
+  },
+  {
+    "revision": "634b1d366ca0bbe50e5c",
+    "url": "/in-our-vue-it-matters/css/chunk-vendors.80a9e6ca.css"
+  },
+  {
+    "revision": "ef8c5fb8bee8772f610759cd3e8a471b",
+    "url": "/in-our-vue-it-matters/img/anarchy.ef8c5fb8.svg"
+  },
+  {
+    "revision": "a7502263a599f6b1da2a447eb954f19b",
+    "url": "/in-our-vue-it-matters/index.html"
+  },
+  {
+    "revision": "6ac2fbf54a637d5f7433",
+    "url": "/in-our-vue-it-matters/js/about.55d175b3.js"
+  },
+  {
+    "revision": "cdaa517a5341d1b0c2be",
+    "url": "/in-our-vue-it-matters/js/app.5ffd8a78.js"
+  },
+  {
+    "revision": "634b1d366ca0bbe50e5c",
+    "url": "/in-our-vue-it-matters/js/chunk-vendors.4b3f4836.js"
+  },
+  {
+    "revision": "ae0d00c67777338042d0e8064be607c5",
+    "url": "/in-our-vue-it-matters/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/in-our-vue-it-matters/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/in-our-vue-it-matters/precache-manifest.3bc955ee4becb0091426dd04de86a7cb.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "in-our-vue-it-matters"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
